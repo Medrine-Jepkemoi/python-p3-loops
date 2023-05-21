b2681f1128caa8a6402a28d47a67240ecc4c2775
Medrine-Jepkemoi <jepkemoimedrine@gmail.com>
2023-05-21 14:12:48 +0300

    Completed Assignment

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,44 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
+
+happy_new_year()
 
 def square_integers(int_list):
     # code goes here!
-    pass
+
+    squares = [i ** 2 for i in int_list]
+    return squares
+    # squares = list()
+    # for i in int_list:
+    #     square = i ** 2
+    #     squares.append(square)
+    #     print(squares)
+
+print(square_integers([1, 2, 3, 4, 5]))
+
 
 def fizzbuzz():
     # code goes here!
-    pass
+    num = 1
+
+    while num <= 100:
+
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz") 
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+        num += 1
+
+fizzbuzz()
+
